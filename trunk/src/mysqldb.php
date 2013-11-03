a9800929fa5737ee829af40786aa6e156d56f864
roztok7@gmail.com <roztok7@gmail.com@fa99eb3e-f746-4683-c29b-0ee65384de34>
2013-11-03 22:57:30 +0000

    abstract classes

--- trunk/src/mysqldb.php
+++ trunk/src/mysqldb.php
@@ -464,3 +464,136 @@ class MysqlDb {
         $this->connect();
     }
 }
+
+abstract class DBClass {
+
+    protected $m_id;
+    protected $isDirty;
+    protected $sqlConn;
+
+    public function __construct(& $sqlConn, $id = 0) {
+        $this->isDirty = False;
+        $this->sqlConn = $sqlConn;
+        
+        if($id) {
+            $this->m_id = $id;
+            $this->load();
+        }
+    }
+
+    abstract public function load($row = Null);
+    abstract public function save();
+
+    public function membersToArray() {
+        $arr = array();
+        foreach(get_object_vars($this) AS $key=>$var) {
+            $varPfx = substr($key, 0, 2);
+            if ($varPfx == "m_") {
+                $arr[substr($key, 2)] = $var;
+            }
+        }
+        return $arr;
+    }
+
+    public function set($name, $value) {
+        if($this->$name != $value) {
+            $this->$name = $value;
+            $this->isDirty = True;
+        }
+    }
+
+    public function toTeng($teng, $res, $fragmentName = Null) {
+        if($fragmentName === Null) {
+            $fragmentName =  strtolower(get_class($this));
+        }
+        return $teng->addFragment($res,$fragmentName, $this->membersToArray());
+    }
+    
+    public function __get($param) {
+        $this->load();
+        return $this->$param;
+    }
+}
+
+class ListObject {
+
+    protected $sqlConn;
+    public $list;
+    protected $filter;
+    protected $join;
+    protected $order;
+    protected $limit;
+    protected $itemClassName;
+    protected $dbTableName;
+
+    public function __construct(& $sqlConn) {
+        $this->sqlConn = $sqlConn;
+        $this->list = array();
+        $this->filter = array();
+        $this->join = "";
+        $this->limit = "";
+        $this->order = "";
+        $this->direction = "";
+    }
+
+    public function setFilter($filter) { $this->filter = $filter; }
+    public function setOrder($order) { $this->order = $order; }
+    public function setDirection($direction) { $this->direction = $direction; }
+    public function setLimit($limit) { $this->limit = $limit; }
+    public function setJoin($join) { $this->join = $join; }
+
+    protected function getWhere() {
+        $where = "";
+        if (count($this->filter)) {
+            $where = "WHERE ";
+            foreach($this->filter AS $key=>$value) {
+                $where .= $this->sqlConn->renderQuery("$key=%s", $value);
+            }
+        }
+        return $where;
+    }
+    
+    protected function getLimit() {
+        if( $this->limit ) {
+            return "LIMIT ".$this->limit;
+        } else {
+            return "";
+        }
+    }
+    
+    protected function getOrder() {
+        if( $this->order ) {
+            return "ORDER ".$this->order." ".$this->direction;
+        } else {
+            return "";
+        }
+    }
+
+    public function load() {
+        $this->sqlConn->begin();
+        $sql = "SELECT * FROM `".$this->dbTableName."` ".$this->join." ".$this->getWhere();
+        $sql .= " ".$this->getOrder." ".$this->getLimit();
+        $res = $this->sqlConn->execute($sql);
+        while ($row = $res->fetch_object()) {
+            $item = new $this->itemClassName($this->sqlConn);
+            $item->load($row);
+            $this->list[] = $item;
+        }
+    }
+
+    public function toTeng($teng, $res) {
+        $res = $teng->addFragment($res, strtolower(get_class($this)), array());
+        foreach($this->list AS $item) {
+            $item->toTeng($teng, $res);
+        }
+
+    }
+
+    public function toHtmlSelectTeng($teng, $res, $selected) {
+        $list = array();
+        foreach($this->list AS $item) {
+            $list[$item->getId()] = $item->getName();
+        }
+        $teng->addHTMLSelect($res, $this->selectName, $list, $selected);
+    }
+}
